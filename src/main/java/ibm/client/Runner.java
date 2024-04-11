8492c9365a8e57160316ed79d8078c6715f724d2
devlopermishra <141336384+devlopermishra@users.noreply.github.com>
2024-04-11 16:55:26 +0530

    Initial commit

---
+++ src/main/java/ibm/client/Runner.java
@@ -0,0 +1,22 @@
+package ibm.client;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import ibm.entity.Student;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Student pra = new Student(2,"Alok","male","noida");
+		Configuration cfg = new Configuration().configure();
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction tx = session.beginTransaction();
+		session.persist(pra);
+		tx.commit();
+	}
+
+}
